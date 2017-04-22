e81d3b83742fc89a28a72f79dc60125105bb9c9f
Floren HENRI <fhenri@e1r11p9.42.fr>
2017-04-22 11:52:25 +0200

    [ADD]messing last stend

--- src/client/components/board/index.js
+++ src/client/components/board/index.js
@@ -27,23 +27,40 @@ function createBoard() {
 function lastStand() {
 
 }
-//TODO manque la diagonalle
+
 function forbidenMove(copy, x, y, pion) {
 
-  if (x - 2 > 0 && y + 2 < 19 && y - 2 > 0 && copy[x][y + 1] === pion &&  copy[x][y + 2] === pion && copy[x - 1][y - 1] === pion && copy[x - 2][y - 2] === pion) {
+  if (x - 2 >= 0 && y + 2 <= 19 && y - 2 >= 0 && copy[x][y + 1] === pion &&  copy[x][y + 2] === pion && copy[x - 1][y - 1] === pion && copy[x - 2][y - 2] === pion) {
     return false
   }
 
-  if (x - 2 > 0 && y + 2 < 19 && y - 2  > 0 && copy[x][y - 1] === pion &&  copy[x][y - 2] === pion && copy[x - 1][y + 1] === pion && copy[x - 2][y + 2] === pion) {
+  if (x - 2 >= 0 && y + 2 <= 19 && y - 2  >= 0 && copy[x][y - 1] === pion &&  copy[x][y - 2] === pion && copy[x - 1][y + 1] === pion && copy[x - 2][y + 2] === pion) {
     return false
   }
 
-  if ( x + 2 < 19 && y + 2 < 19 && y - 2  > 0 && copy[x][y + 1] === pion &&  copy[x][y + 2] === pion && copy[x + 1][y - 1] === pion && copy[x + 2][y - 2] === pion) {
+  if ( x + 2 <= 19 && y + 2 <= 19 && y - 2  >= 0 && copy[x][y + 1] === pion &&  copy[x][y + 2] === pion && copy[x + 1][y - 1] === pion && copy[x + 2][y - 2] === pion) {
     return false
   }
-  if ( x + 2 < 19 && y + 2 < 19 && y - 2  > 0 && copy[x][y - 1] === pion &&  copy[x][y - 2] === pion && copy[x + 1][y + 1] === pion && copy[x + 2][y + 2] === pion) {
+  if ( x + 2 <= 19 && y + 2 <= 19 && y - 2  >= 0 && copy[x][y - 1] === pion &&  copy[x][y - 2] === pion && copy[x + 1][y + 1] === pion && copy[x + 2][y + 2] === pion) {
     return false
   }
+
+  if (x + 2 <= 19 && x - 2 >= 0 && y - 2 >= 0 && copy[x - 1][y] === pion &&  copy[x - 2][y] === pion && copy[x + 1][y - 1] === pion && copy[x + 2][y - 2] === pion) {
+    return false
+  }
+
+  if (x - 2 >= 0 && x + 2 <= 19 && y + 2 <= 19 && copy[x - 1][y] === pion &&  copy[x - 2][y] === pion && copy[x + 1][y + 1] === pion && copy[x + 2][y + 2] === pion) {
+    return false
+  }
+
+  if (x + 2 <= 19 && x - 2 >= 0 && y + 2 <= 19 && copy[x + 1][y] === pion &&  copy[x + 2][y] === pion && copy[x - 1][y + 1] === pion && copy[x - 2][y + 2] === pion) {
+    return false
+  }
+
+  if (x + 2 <= 19 && x - 2 >= 0 && y - 2 >= 0 && copy[x + 1][y] === pion &&  copy[x + 2][y] === pion && copy[x - 1][y - 1] === pion && copy[x - 2][y - 2] === pion) {
+    return false
+  }
+
 
   return true
 
@@ -51,70 +68,69 @@ function forbidenMove(copy, x, y, pion) {
 
 function findLine(copy, x, y, pion) {
 
-  if ( x + 4 < 19 && copy[x][y] === pion && copy[x + 1][y] === pion && copy[x + 2][y] === pion && copy[x + 3][y] === pion && copy[x + 4][y] === pion) {
+  if ( x + 4 <= 19 && copy[x][y] === pion && copy[x + 1][y] === pion && copy[x + 2][y] === pion && copy[x + 3][y] === pion && copy[x + 4][y] === pion) {
     return true
   }
-  if ( x - 4 > 0 && copy[x][y] === pion && copy[x - 1][y] === pion && copy[x - 2][y] === pion && copy[x - 3][y] === pion && copy[x - 4][y] === pion) {
+  if ( x - 4 >= 0 && copy[x][y] === pion && copy[x - 1][y] === pion && copy[x - 2][y] === pion && copy[x - 3][y] === pion && copy[x - 4][y] === pion) {
     return true
   }
-  if ( y + 4 < 19 && copy[x][y] === pion && copy[x][y + 1] === pion && copy[x][y + 2] === pion && copy[x][y + 3] === pion && copy[x][y + 4] === pion) {
+  if ( y + 4 <= 19 && copy[x][y] === pion && copy[x][y + 1] === pion && copy[x][y + 2] === pion && copy[x][y + 3] === pion && copy[x][y + 4] === pion) {
     return true
   }
-  if ( y - 4 > 0 && copy[x][y] === pion && copy[x][y - 1] === pion && copy[x][y - 2] === pion && copy[x][y - 3] === pion && copy[x][y - 4] === pion) {
-    return true
-  }
-
-
-  if ( y + 4 < 19 && x + 4 < 19 && copy[x][y] === pion && copy[x + 1][y + 1] === pion && copy[x + 2][y + 2] === pion && copy[x + 3][y + 3] === pion && copy[x + 4][y + 4] === pion) {
-    return true
-  }
-  if ( y - 4 > 0 && x - 4 > 0 && copy[x][y] === pion && copy[x - 1][y - 1] === pion && copy[x - 2][y - 2] === pion && copy[x - 3][y - 3] === pion && copy[x - 4][y - 4] === pion) {
+  if ( y - 4 >= 0 && copy[x][y] === pion && copy[x][y - 1] === pion && copy[x][y - 2] === pion && copy[x][y - 3] === pion && copy[x][y - 4] === pion) {
     return true
   }
 
-  if ( y + 4 < 19 && x - 4 > 0 && copy[x][y] === pion && copy[x - 1][y + 1] === pion && copy[x - 2][y + 2] === pion && copy[x - 3][y + 3] === pion && copy[x - 4][y + 4] === pion) {
+
+  if ( y + 4 <= 19 && x + 4 <= 19 && copy[x][y] === pion && copy[x + 1][y + 1] === pion && copy[x + 2][y + 2] === pion && copy[x + 3][y + 3] === pion && copy[x + 4][y + 4] === pion) {
+    return true
+  }
+  if ( y - 4 >= 0 && x - 4 >= 0 && copy[x][y] === pion && copy[x - 1][y - 1] === pion && copy[x - 2][y - 2] === pion && copy[x - 3][y - 3] === pion && copy[x - 4][y - 4] === pion) {
     return true
   }
 
-  if ( y - 4 > 0 && x + 4 < 19 && copy[x][y] === pion && copy[x + 1][y - 1] === pion && copy[x + 2][y - 2] === pion && copy[x + 3][y - 3] === pion && copy[x + 4][y - 4] === pion) {
+  if ( y + 4 <= 19 && x - 4 >= 0 && copy[x][y] === pion && copy[x - 1][y + 1] === pion && copy[x - 2][y + 2] === pion && copy[x - 3][y + 3] === pion && copy[x - 4][y + 4] === pion) {
+    return true
+  }
+
+  if ( y - 4 >= 0 && x + 4 <= 19 && copy[x][y] === pion && copy[x + 1][y - 1] === pion && copy[x + 2][y - 2] === pion && copy[x + 3][y - 3] === pion && copy[x + 4][y - 4] === pion) {
     return true
   }
   false
 }
 function findCapture(copy,x, y, pion, pion2) {
 
-  if ( x + 3 < 19 && copy[x + 3][y] === pion && copy[x + 1][y] === pion2 && copy[x + 2][y] === pion2) {
+  if ( x + 3 <= 19 && copy[x + 3][y] === pion && copy[x + 1][y] === pion2 && copy[x + 2][y] === pion2) {
     return { x1: x + 1, y1: y, x2: x + 2,y2:y }
   }
 
-  if ( x - 3 > 0 &&  copy[x - 3][y] === pion && copy[x - 1][y] === pion2 && copy[x - 2][y] === pion2) {
+  if ( x - 3 >= 0 &&  copy[x - 3][y] === pion && copy[x - 1][y] === pion2 && copy[x - 2][y] === pion2) {
     return { x1: x - 1, y1: y, x2: x - 2, y2: y }
   }
 
-  if ( y + 3 < 19 && copy[x][y + 3] === pion && copy[x][y + 1] === pion2 && copy[x][y + 2] === pion2) {
+  if ( y + 3 <= 19 && copy[x][y + 3] === pion && copy[x][y + 1] === pion2 && copy[x][y + 2] === pion2) {
     return { x1: x, y1: y + 1 , x2: x, y2:y + 2 }
   }
 
-  if ( y - 3 > 0 && copy[x][y - 3] === pion && copy[x][y - 1] === pion2 && copy[x][y - 2] === pion2) {
+  if ( y - 3 >= 0 && copy[x][y - 3] === pion && copy[x][y - 1] === pion2 && copy[x][y - 2] === pion2) {
     return { x1: x, y1: y - 1, x2: x, y2: y - 2 }
   }
 
-  if ( x + 3 < 19 && y + 3 < 19 && copy[x + 3][y + 3] === pion && copy[x + 1][y + 1] === pion2 && copy[x + 2][y + 2] === pion2) {
+  if ( x + 3 <= 19 && y + 3 <= 19 && copy[x + 3][y + 3] === pion && copy[x + 1][y + 1] === pion2 && copy[x + 2][y + 2] === pion2) {
     return { x1: x + 1, y1: y + 1, x2: x + 2, y2: y + 2 }
   }
 
-  if ( x - 3 > 0 && y - 3 > 0 && copy[x - 3][y - 3] === pion && copy[x - 1][y - 1] === pion2 && copy[x - 2][y - 2] === pion2) {
+  if ( x - 3 >= 0 && y - 3 >= 0 && copy[x - 3][y - 3] === pion && copy[x - 1][y - 1] === pion2 && copy[x - 2][y - 2] === pion2) {
     return { x1: x - 1, y1: y - 1, x2: x - 2, y2: y - 2 }
   }
 
-  if ( x - 3 > 0 && y + 3 < 19 && copy[x - 3][y + 3] === pion && copy[x - 1][y + 1] === pion2 && copy[x - 2][y + 2] === pion2) {
+  if ( x - 3 >= 0 && y + 3 <= 19 && copy[x - 3][y + 3] === pion && copy[x - 1][y + 1] === pion2 && copy[x - 2][y + 2] === pion2) {
     return { x1: x - 1, y1: y + 1, x2: x - 2, y2: y + 2 }
   }
 
-  if ( x + 3 < 19 && y - 3 > 0 && copy[x + 3][y - 3] === pion && copy[x + 1][y - 1] === pion2 && copy[x + 2][y - 2] === pion2) {
+  if ( x + 3 <= 19 && y - 3 >= 0 && copy[x + 3][y - 3] === pion && copy[x + 1][y - 1] === pion2 && copy[x + 2][y - 2] === pion2) {
     return { x1: x + 1, y1: y - 1, x2: x + 2, y2: y - 2 }
   }
-
 
 
   return false
