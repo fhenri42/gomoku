9a64794280e25c53fbd71e2e146beddfce875e2a
Floren HENRI <fhenri@e1r11p9.42.fr>
2017-04-25 11:56:15 +0200

    [ADD]script in go

--- src/server/event/index.js
+++ src/server/event/index.js
@@ -1,10 +1,24 @@
+require('node-go-require')
 import _ from 'lodash'
+
 
 export default (socket) => {
   socket.on('activation',(data) => aiTurn(data,socket))
 }
 
 const aiTurn = (data, socket) => {
+
+try {
+
+  var petGo = require("./main.go");
+
+  // var pet = petGo.pet.New('my pet');
+  // console.log(pet.Name());
+  // pet.SetName('new name...');
+  // console.log(pet.Name());
+} catch (e){
+  console.log(e);
+}
   socket.emit('action', {
        type: 'AITURN',
        data,
