11a94e986063d5c4269f40b4c2ec154c10d6dd6f
Bruno GANTELMI <bgantelm@e1r11p10.42.fr>
2017-05-03 15:51:50 +0200

    ADD

--- srcs/isPlayable.go
+++ srcs/isPlayable.go
@@ -1,10 +1,7 @@
 package main
 
 func isPlayable(tools *sdlTools, x int, y int) bool {
-  if (x >= SIZE || y >= SIZE || x < 0 || y < 0 || tools.board[x][y] != 0) {
-    return false
-  }
-  if (!isForbidenMove(tools, x, y)) {
+  if (x >= SIZE || y >= SIZE || x < 0 || y < 0 || tools.board[x][y] != 0 || isForbidenMove(tools, x, y)) {
     return false
   }
   return true
