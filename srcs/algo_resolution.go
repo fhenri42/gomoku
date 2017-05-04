c69eef3d161222e077ef275c8404fa0f3a12dcfd
Floren HENRI <fhenri@e1r11p9.42.fr>
2017-05-04 12:03:27 +0200

    [ADD] goroutine add on algo

--- srcs/algo_resolution.go
+++ srcs/algo_resolution.go
@@ -10,20 +10,13 @@ var CALCULE_VALUE_ROUTINE sync.WaitGroup
 
 func  findCoup(index int, coups[]move,coup *move, aiScore int, playerScore int, board*[SIZE][SIZE]int, player int)  {
 
-
   var tmpBoard [SIZE][SIZE]int
   var tmpPlayerScore int
   var tmpAiScore int
 
-  CALCULE_VALUE_ROUTINE.Add(DEPTH_MAX)
-
   tmpAiScore, tmpPlayerScore = aiScore, playerScore
   tmpBoard = moveAndEat(*board, coup.x, coup.y, player, &tmpAiScore, &tmpPlayerScore)
-  //coups[index].poid = go calcValue(tmpBoard, 1, tmpAiScore, tmpPlayerScore, player, *coups[index].poid)
-  go calcValue(tmpBoard, 1, tmpAiScore, tmpPlayerScore, player, &coups[index].poid)
-
-
-  CALCULE_VALUE_ROUTINE.Wait()
+  coups[index].poid = calcValue(tmpBoard, 1, tmpAiScore, tmpPlayerScore, player)
 
   fmt.Println("this is takin time like hell lot of time",coup)
   defer FIND_COUP_ROUTINE.Done()
@@ -52,8 +45,7 @@ func getBestMove(board [SIZE][SIZE]int, aiScore int, playerScore int, player int
   return tmp
 }
 
-func calcValue(board [SIZE][SIZE]int, depth int, aiScore int, playerScore int, player int, test* int) int {
-  defer CALCULE_VALUE_ROUTINE.Done()
+func calcValue(board [SIZE][SIZE]int, depth int, aiScore int, playerScore int, player int) int {
   end, winner := endGame(board, playerScore, aiScore) // OBSERVE LA GRILLE POUR SAVOIR SI C'EST LA FIN
   var tmpBoard [SIZE][SIZE]int
   var tmpPlayerScore int
@@ -63,17 +55,13 @@ func calcValue(board [SIZE][SIZE]int, depth int, aiScore int, playerScore int, p
 
   if (end) { // SI FIN DU GAME
     if (winner == EQUAL) { // SI MATCH NUL
-      *test = 0
       return 0
     } else if (winner == PLAYER2) { // SI P2 GAGNANT
-      *test = MIN_BASE - depth
       return MIN_BASE - depth
     } else if (winner == PLAYER1) { // SI P1 GAGNANT
-      *test = MAX_BASE + depth
       return MAX_BASE + depth
     }
   } else if (depth == DEPTH_MAX) { // SINON SI ON EST ARRIVEE A LA PROFONDEUR MAX
-    *test = evaluate(board, aiScore, playerScore)
     return evaluate(board, aiScore, playerScore) // ON EVALUE LES NOEUDS FINAUX GRACE A UNE HEURISTIQUE
   } else { // SINON ON CALCULE EN ALTERNANCE MIN/MAX PAR RAPPORT AUX EVALUATIONS DES NOEUD FINAUX EN REMONTANT JUSQUA PROFONDEUR 0
 
@@ -90,20 +78,15 @@ func calcValue(board [SIZE][SIZE]int, depth int, aiScore int, playerScore int, p
  for index,coup := range coups {
    tmpAiScore, tmpPlayerScore = aiScore, playerScore
    tmpBoard = moveAndEat(board, coup.x, coup.y, player, &tmpAiScore, &tmpPlayerScore)
-   //coups[index].poid = go calcValue(tmpBoard, depth + 1, tmpAiScore, tmpPlayerScore, player)
-    go calcValue(tmpBoard, depth + 1, tmpAiScore, tmpPlayerScore, player, &coups[index].poid)
-
+   coups[index].poid = calcValue(tmpBoard, depth + 1, tmpAiScore, tmpPlayerScore, player).
  }
 
     if (depth % 2 == 1) {
-      *test = minCoup(coups).poid
       return minCoup(coups).poid
     } else {
-      *test = maxCoup(coups).poid
       return maxCoup(coups).poid
     }
   }
-  *test = 0
   return 0
 }
 
