0fe5d93a6183e8202c1963e081a69b65a291ee23
Floren HENRI <fhenri@e1r11p9.42.fr>
2017-05-05 17:35:23 +0200

    [ADD]change to search

--- srcs/algo_expander.go
+++ srcs/algo_expander.go
@@ -1,47 +1,106 @@
 package main
 
-func isClose(x int, y int, board[SIZE][SIZE]int, movesOfone*[]move, movesOfTwo *[]move) {
-  if (board[x][y] != 0) {
-    return
+import (
+  "fmt"
+)
+
+  type afluence struct {
+    conteur int
+    arrayMove move
   }
-  var i int = -AMP
-  for i <= AMP {
-    var j int = -AMP
-    for j <= AMP {
-        if ((x + i) + i >= 0 && (x + i) + i < SIZE && (y + j) + j >= 0 && (y + j) + j < SIZE && board[x + i][y + j] != 0 && board[(x + i) + i][( y + j) + j] != 0) {
-          *movesOfTwo = append(*movesOfTwo, newMove(x, y, 0))
-          return
-        }
+
+
+  func isClose(x int, y int, board[SIZE][SIZE]int) afluence {
+
+
+    var conteur int
+    conteur = 0
+    var i int = -AMP
+    for i <= AMP {
+      var j int = -AMP
+      for j <= AMP {
         if (x + i >= 0 && x + i < SIZE && y + j >= 0 && y + j < SIZE && board[x + i][y + j] != 0) {
-          *movesOfone = append(*movesOfone, newMove(x, y, 0))
-        return
+          conteur++
+        }
+        j++
       }
-      j++
+      i++
     }
-    i++
-  }
-  return
-}
-
-func findMoves(tools *sdlTools) ([]move) {
-
-  movesOfone := make([]move, 0)
-  movesOfTwo := make([]move, 0)
-
-  var x int = 0
-  for x < SIZE {
-    var y int = 0
-    for y < SIZE {
-      if isPlayable(tools,x,y) {
-        isClose(x,y, tools.board, &movesOfone, &movesOfTwo)
-      }
-      y++
-    }
-    x++
+    var tmp afluence
+    tmp.conteur = conteur
+    tmp.arrayMove = newMove(x, y, 0)
+    return tmp
   }
 
-if len(movesOfTwo) > 0  {
-  return movesOfTwo
-}
-  return movesOfone
-}
+  func findMoves(tools *sdlTools) ([]move) {
+
+    arrayAflu := make([]afluence, 0)
+
+    var x int = 0
+    for x < SIZE {
+      var y int = 0
+      for y < SIZE {
+        if tools.board[x][y] == 0 {
+          arrayAflu = append(arrayAflu, isClose(x,y, tools.board))
+         }
+        y++
+      }
+      x++
+    }
+
+    tmpBoard := make([]move, 0)
+    var tmpConter int = 0
+    var t int = 0
+    for t < len(arrayAflu) {
+      if (tmpConter < arrayAflu[t].conteur) {
+        tmpConter = arrayAflu[t].conteur
+      }
+        t++
+      }
+      t = 0
+      for t < len(arrayAflu) {
+        if tmpConter == arrayAflu[t].conteur {
+          tmpBoard = append(tmpBoard, arrayAflu[t].arrayMove)
+        }
+        t++
+      }
+      fmt.Println("tmpBoard==",tmpBoard)
+      return tmpBoard
+    }
+
+//ancinne version
+
+// func isClose(x int, y int, board[SIZE][SIZE]int) bool {
+//   if (board[x][y] != 0) {
+//     return false
+//   }
+//   var i = -AMP
+//   for i <= AMP {
+//     var j = -AMP
+//     for j <= AMP {
+//       if (x + i >= 0 && x + i < SIZE && y + j >= 0 && y + j < SIZE && board[x + i][y + j] != 0) {
+//         return true
+//       }
+//       j++
+//     }
+//     i++
+//   }
+//   return false
+// }
+//
+// func findMoves(board[SIZE][SIZE]int) ([]move) {
+//
+//   moves := make([]move, 0)
+//   var x = 0
+//   for x < SIZE {
+//     var y = 0
+//     for y < SIZE {
+//       if (isClose(x, y, board)) {
+//         moves = append(moves, newMove(x, y, 0))
+//       }
+//       y++
+//     }
+//     x++
+//   }
+//   return moves
+// }
