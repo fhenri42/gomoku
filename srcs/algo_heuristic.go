3d7dd7dca2475a9155cae28ac7f094aee088f2e2
Hugo CHERCHI <hcherchi@e1r11p10.42.fr>
2017-05-10 17:13:08 +0200

    [UPD]

--- srcs/algo_heuristic.go
+++ srcs/algo_heuristic.go
@@ -5,10 +5,12 @@ import (
 
 const EATABLE_MALUS = 10
 const SCORE_BONUS = 1
-const SPACE_5_BONUS = 10
-const SPACE_9_BONUS = 10
-const SUP_SPACE_BONUS = 2
-const ALIGNED_BONUS = 5
+const FREE_FOUR_BONUS = 50
+const FULL_FREE_BONUS = 20
+const LINE_BONUS = 5
+const SPACE_BONUS = 2
+const COEF_FREE = 1
+const COEF_FRIEND = 3
 
 // Evaluate a grid
 func evaluate(game *Game) Move {
@@ -49,7 +51,9 @@ func evaluateEach(board *[SIZE][SIZE]int, x int, y int) int {
   var player = board[x][y]
   var res int = 0
   var countFree int = 0
+  var countLine int = 0
   var countFriend int = 0
+  var limit bool = false
 
   if (isEatable(x, y, board)) {
     res -= EATABLE_MALUS
@@ -61,43 +65,57 @@ func evaluateEach(board *[SIZE][SIZE]int, x int, y int) int {
 
       countFree = 0
       countFriend = 0
+      countLine = 0
+      limit = false
 
       k = -1
       for (k >= -4 && x + i * k >= 0 && x + i * k < SIZE && y + j * k >= 0 && y + j * k < SIZE && (*board)[x + i * k][y + j * k] != player % 2 + 1) {
+
+        if ((*board)[x + i * k][y + j * k] == player && !limit) {
+          countLine++
+        } else if ((*board)[x + i * k][y + j * k] == NONE && !limit) {
+          limit = true
+        }
+
         if ((*board)[x + i * k][y + j * k] == player) {
           countFriend++
         } else {
           countFree++
         }
+
         k--
       }
 
       k = 1
       for (k <= 4 && x + i * k >= 0 && x + i * k < SIZE && y + j * k >= 0 && y + j * k < SIZE && (*board)[x + i * k][y + j * k] != player % 2 + 1) {
+
+        if ((*board)[x + i * k][y + j * k] == player && !limit) {
+          countLine++
+        } else if ((*board)[x + i * k][y + j * k] == NONE && !limit) {
+          limit = true
+        }
+
         if ((*board)[x + i * k][y + j * k] == player) {
           countFriend++
         } else {
           countFree++
         }
+
         k++
       }
 
+
       if (countFree + countFriend >= 4) {
-        res += SPACE_5_BONUS
-        if (countFree + countFriend >= 8) {
-          res += SPACE_9_BONUS
+
+        res += SPACE_BONUS * (countFree * COEF_FREE + countFriend * COEF_FRIEND)
+        res += LINE_BONUS * countLine
+        if (countFree + countFriend >= 6 && countLine >= 3) {
+          res += FREE_FOUR_BONUS
         }
-        k = 0
-        for k + 4 < countFree + countFriend {
-          res += SUP_SPACE_BONUS
-          k++
+        if (countFree + countFriend >= 8) {
+          res += FULL_FREE_BONUS
         }
 
-        k = 0
-        for k < countFriend {
-          res += ALIGNED_BONUS
-          k++
-        }
       }
 
 
