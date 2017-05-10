bacac949a252126edf7d11ad7dde9d1214d1030e
Hugo CHERCHI <hcherchi@e1r11p10.42.fr>
2017-05-10 11:00:55 +0200

    [UPD]: heuristic

--- srcs/algo_heuristic.go
+++ srcs/algo_heuristic.go
@@ -10,12 +10,12 @@ func evaluate(game *Game) Move {
   var j int = 0
 
   for i < game.score[game.friend - 1] {
-    res += i + 1 * 10
+    res += int(math.Pow(float64(i), 2))
     i++
   }
   i = 0
   for i < game.score[game.friend % 2] {
-    res -= i + 1 * 10
+    res -= int(math.Pow(float64(i), 2))
     i++
   }
   i = 0
@@ -41,28 +41,56 @@ func evaluateEach(board *[SIZE][SIZE]int, x int, y int) int {
   var k int = 1
   var player = board[x][y]
   var res int = 0
-  var count int = 0
+  var countFree int = 0
+  var countFriend int = 0
+
 
   for i <= 1 {
     j = -1
     for j <= 1 {
+
+      countFree = 0
+      countFriend = 0
+
+      k = -1
+      for (k >= -4 && x + i * k >= 0 && x + i * k < SIZE && y + j * k >= 0 && y + j * k < SIZE && (*board)[x + i * k][y + j * k] != player % 2 + 1) {
+        if ((*board)[x + i * k][y + j * k] == player) {
+          countFriend++
+        } else {
+          countFree++
+        }
+        k--
+      }
+
       k = 1
-      count = 0
-      for k <= 4 {
-        if (k == 0 || (x + i * k >= 0 && x + i * k < SIZE && y + j * k >= 0 && y + j * k < SIZE && (*board)[x + i * k][y + j * k] == player)) {
-          count++
+      for (k <= 4 && x + i * k >= 0 && x + i * k < SIZE && y + j * k >= 0 && y + j * k < SIZE && (*board)[x + i * k][y + j * k] != player % 2 + 1) {
+        if ((*board)[x + i * k][y + j * k] == player) {
+          countFriend++
+        } else {
+          countFree++
         }
         k++
       }
+
+      if (countFree + countFriend >= 5) {
+        k = 0
+        for k < countFree {
+          res += i
+          k++
+        }
+
+        k = 0
+        for k < countFree {
+          res += int(math.Pow(float64(k + 1), 2))
+          k++
+        }
+      }
+
+
       j++
     }
     i++
   }
 
-  i = 0
-  for i < count {
-    res += int(math.Pow(float64(i), 10))
-    i++
-  }
   return res
 }
